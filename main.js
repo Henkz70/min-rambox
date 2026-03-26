9f55fe9f75ef08fa79327eeadd5495b5ebc8e119
Henkz70 <teknik@vlmedia.se>
2026-03-26 10:33:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      webviewTag: true, // Viktigt för att kunna bädda in webbsidor
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
